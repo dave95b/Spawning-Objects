1f66b5e4c264f4410ebc7aa050d40442c135cc0d
Dave <dave95.b@gmail.com>
2019-06-09 12:37:13 +0200

    Added custom shader and random colors

--- Assets/Scripts/Shape.cs
+++ Assets/Scripts/Shape.cs
@@ -9,9 +9,25 @@ namespace Core
         [SerializeField]
         private MeshRenderer renderer;
 
+        private static int colorPropertyId = Shader.PropertyToID("_Color");
+        private static MaterialPropertyBlock propertyBlock;
+
         public Material Material
         {
             set => renderer.material = value;
+        }
+
+        public Color Color
+        {
+            set
+            {
+                //renderer.material.color = value;
+                if (propertyBlock is null)
+                    propertyBlock = new MaterialPropertyBlock();
+
+                propertyBlock.SetColor(colorPropertyId, value);
+                renderer.SetPropertyBlock(propertyBlock);
+            }
         }
 
         private void Reset()
---
+++ Assets/Scripts/Spawners/Listeners/ColorChanger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using SpawnerSystem.Spawners;
+
+namespace Core.Spawners.Listeners
+{
+    public class ColorChanger : MonoBehaviour, ISpawnListener<Shape>
+    {
+        public void OnSpawned(Shape spawned)
+        {
+            spawned.Color = Random.ColorHSV(
+                hueMin: 0f, hueMax: 1f,
+                saturationMin: 0.5f, saturationMax: 1f,
+                valueMin: 0.25f, valueMax: 1f,
+                alphaMin: 1f, alphaMax: 1f);
+        }
+
+        public void OnDespawned(Shape despawned) { }
+    }
+}
