8496e7458f09cb5cda6ff771017458459c1d8f7f
Dave <dave95.b@gmail.com>
2019-08-22 08:25:53 +0200

    Updated Unity

--- Assets/Plugins/Spawner System/Plugins/ExtensionMethods/CollectionExtensions.cs
+++ Assets/Plugins/Spawner System/Plugins/ExtensionMethods/CollectionExtensions.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class CollectionExtensions {
+public static class CollectionExtensions
+{
 
 #region IList
 
-	public static void Shuffle<T>(this IList<T> list) {
+	public static void Shuffle<T>(this IList<T> list)
+    {
 		int count = list.Count;
 		var last = count - 1;
-		for (int i = 0; i < last; ++i) {
+
+		for (int i = 0; i < last; ++i)
+        {
 			int randomIndex = Random.Range(i, count);
 			T tmp = list[i];
 			list[i] = list[randomIndex];
@@ -17,15 +21,13 @@ public static class CollectionExtensions {
 		}
 	}
 
-	public static T GetRandom<T>(this IList<T> list) {
+	public static T GetRandom<T>(this IList<T> list)
+    {
 		return list[Random.Range(0, list.Count)];
 	}
 
-	public static bool IsEmpty<T>(this IList<T> list) {
-		return list.Count == 0;
-	}
-
-	public static void Swap<T>(this IList<T> list, int sourceIndex, int destinationIndex) {
+	public static void Swap<T>(this IList<T> list, int sourceIndex, int destinationIndex)
+    {
         if (sourceIndex == destinationIndex)
             return;
 
@@ -45,10 +47,13 @@ public static class CollectionExtensions {
 
 #region Arrays
 
-	public static void Shuffle<T>(this T[] array) {
+	public static void Shuffle<T>(this T[] array)
+    {
 		int count = array.Length;
 		var last = count - 1;
-		for (int i = 0; i < last; ++i) {
+
+		for (int i = 0; i < last; ++i)
+        {
 			int randomIndex = Random.Range(i, count);
 			T tmp = array[i];
 			array[i] = array[randomIndex];
@@ -56,24 +61,11 @@ public static class CollectionExtensions {
 		}
 	}
 
-	public static T GetRandom<T>(this T[] array){
+	public static T GetRandom<T>(this T[] array)
+    {
 		return array[Random.Range(0, array.Length)];
 	}
 
-	public static bool IsEmpty<T>(this T[] array) {
-		if (array.Length == 0) return true;
-
-		for (int i = 0; i < array.Length; i++)
-			if (array[i] != null)
-				return false;
-
-		return true;
-	}
-
-	public static void Clear<T>(this T[] array) {
-        for (int i = 0; i < array.Length; i++)
-            array[i] = default(T);
-    }
 
     public static void Swap<T>(this T[] array, int sourceIndex, int destinationIndex)
     {
